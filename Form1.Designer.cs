3d70387fce0fb287bd38c598236ee170bf22982e
Roman <martanovroman741@gmail.com>
2024-09-03 23:14:48 +0400

    first version of program

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,10 +30,12 @@
         {
             this.components = new System.ComponentModel.Container();
             this.labelStatus = new System.Windows.Forms.Label();
-            this.buttonStart = new System.Windows.Forms.Button();
             this.textBoxTime = new System.Windows.Forms.TextBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.GitHubButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonGitHub = new System.Windows.Forms.Button();
+            this.buttonStart = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // labelStatus
@@ -44,48 +46,75 @@
             this.labelStatus.AutoSize = true;
             this.labelStatus.Cursor = System.Windows.Forms.Cursors.IBeam;
             this.labelStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.labelStatus.Location = new System.Drawing.Point(62, 9);
+            this.labelStatus.Location = new System.Drawing.Point(64, 189);
             this.labelStatus.Name = "labelStatus";
             this.labelStatus.Size = new System.Drawing.Size(360, 32);
             this.labelStatus.TabIndex = 0;
             this.labelStatus.Text = "Введите время в минутах.";
-            // 
-            // buttonStart
-            // 
-            this.buttonStart.Location = new System.Drawing.Point(204, 223);
-            this.buttonStart.Name = "buttonStart";
-            this.buttonStart.Size = new System.Drawing.Size(75, 23);
-            this.buttonStart.TabIndex = 1;
-            this.buttonStart.Text = "button1";
-            this.buttonStart.UseVisualStyleBackColor = true;
-            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            this.labelStatus.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // textBoxTime
             // 
-            this.textBoxTime.Location = new System.Drawing.Point(322, 302);
+            this.textBoxTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxTime.Location = new System.Drawing.Point(138, 352);
             this.textBoxTime.Name = "textBoxTime";
-            this.textBoxTime.Size = new System.Drawing.Size(100, 22);
+            this.textBoxTime.Size = new System.Drawing.Size(206, 53);
             this.textBoxTime.TabIndex = 2;
             // 
             // timer1
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
-            // GitHubButton
+            // button1
             // 
-            this.GitHubButton.Location = new System.Drawing.Point(379, 72);
-            this.GitHubButton.Name = "GitHubButton";
-            this.GitHubButton.Size = new System.Drawing.Size(65, 65);
-            this.GitHubButton.TabIndex = 3;
-            this.GitHubButton.Text = "button1";
-            this.GitHubButton.UseVisualStyleBackColor = true;
+            this.button1.BackgroundImage = global::Timer.Properties.Resources.stop_button;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(350, 321);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 120);
+            this.button1.TabIndex = 5;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.BackgroundImage = global::Timer.Properties.Resources.no;
+            this.buttonExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonExit.Location = new System.Drawing.Point(12, 12);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(70, 70);
+            this.buttonExit.TabIndex = 4;
+            this.buttonExit.UseVisualStyleBackColor = true;
+            // 
+            // buttonGitHub
+            // 
+            this.buttonGitHub.BackgroundImage = global::Timer.Properties.Resources.github_logo;
+            this.buttonGitHub.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonGitHub.Location = new System.Drawing.Point(400, 12);
+            this.buttonGitHub.Name = "buttonGitHub";
+            this.buttonGitHub.Size = new System.Drawing.Size(70, 70);
+            this.buttonGitHub.TabIndex = 3;
+            this.buttonGitHub.UseVisualStyleBackColor = true;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackgroundImage = global::Timer.Properties.Resources.play_button;
+            this.buttonStart.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonStart.Location = new System.Drawing.Point(12, 321);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(120, 120);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(482, 453);
-            this.Controls.Add(this.GitHubButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonGitHub);
             this.Controls.Add(this.textBoxTime);
             this.Controls.Add(this.buttonStart);
             this.Controls.Add(this.labelStatus);
@@ -103,7 +132,9 @@
         private System.Windows.Forms.Button buttonStart;
         private System.Windows.Forms.TextBox textBoxTime;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Button GitHubButton;
+        private System.Windows.Forms.Button buttonGitHub;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button button1;
     }
 }
 
